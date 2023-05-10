f96528010fb1fe7656e39b13c531d98103e8d95c
najvoa <neenu.ajilkumar@scania.com>
2023-05-10 11:52:16 +0200

    solved day1_part1

---
+++ day1.py
@@ -0,0 +1,23 @@
+f = open("day1_input.txt")
+
+# input_list = f.readlines()
+
+lines = f.read().strip().split("\n")
+lines.append('')
+
+elvs = []
+sum_calories = 0
+
+for line in lines:
+    if line == "":
+        # we reached the end of an elv's inventory
+        # save the sum of calories to elvs
+        elvs.append(sum_calories)
+        sum_calories = 0
+    else:
+        # sum the calories
+        calories = int(line)
+        sum_calories = sum_calories + calories
+
+
+print(max(elvs))
